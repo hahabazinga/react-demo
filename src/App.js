0fb291037d000d45a10ac39e0313b682f99a0dcd
wangshuai <wangshuai@momenta.ai>
2018-12-27 18:01:47 +0800

    pwa test

--- src/App.js
+++ src/App.js
@@ -4,13 +4,32 @@ import './App.css';
 
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      country: ''
+    }
+    this.getCountry = this.getCountry.bind(this);
+  }
+  getCountry() {
+    fetch('https://ipapi.co/8.8.8.8/json/').then(response => response.json()).then(data => {
+
+      this.setState({
+          text:data.country_name
+      })
+     
+    }).catch(e => alert(e.message));
+  }
 
   render() {
 
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">edit</h1>
+          <h1 className="App-title">
+          <button onClick={() => this.getCountry()}>swTest</button>
+          <h3>{this.state.country}</h3>
+          </h1>
         </header>
         <Main/>
 
